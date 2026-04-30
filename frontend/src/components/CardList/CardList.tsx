2cd1e7844b605bdc0125decd566121469a5d497f
brunoseleguim <seleguimbruno900@gmail.com>
2026-04-30 10:03:57 -0300

    terceiro commit

---
+++ frontend/src/components/CardList/CardList.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from '../card/Card'
+
+interface Props {}
+
+// Mudança aqui também: Tipagem profissional
+const CardList: React.FC<Props> = (props: Props): React.JSX.Element => {
+  return (
+    <div>
+      <Card companyName="Apple" ticker="AAPL" price={110} />
+      <Card companyName="Microsoft" ticker="MSFT" price={200} />
+      <Card companyName="Tesla" ticker="TSLA" price={300} />
+    </div>
+  )
+}
+
+export default CardList
--- frontend/src/components/card/Card.tsx
+++ frontend/src/components/card/Card.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React from 'react'
 import "./card.css";
-interface Props {}
 
-const Card = (props: Props) => {
+interface Props {
+  companyName: string;
+  ticker: string;
+  price: number;
+}
+
+// Mudança aqui: Tipagem da função e do retorno
+const Card: React.FC<Props> = ({ companyName, ticker, price }: Props): React.JSX.Element => {
   return (
     <div className="card">
       <img
-        src="https://s3-symbol-logo.tradingview.com/apple--600.png"
-        alt="Apple Stock"
+        src="https://s.yimg.com/ny/api/res/1.2/Fe2ID8yI9erQgqpNh.la9Q--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM2Ng--/https://media.zenfs.com/en/the_motley_fool_261/9558c5bc1963a96d67c909460a526749"
+        alt="Stock"
       />
       <div className="details">
-        <h2>Apple</h2>
-        <p>$110</p>
+        <h2>{companyName} ({ticker})</h2>
+        <p>${price}</p>
       </div>
       <p className="info">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       </p>
     </div>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
