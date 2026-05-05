463c557fa8589390b0bab609938f921176abd6f6
brunoseleguim <seleguimbruno900@gmail.com>
2026-05-05 12:33:53 -0300

    decimo quinto commit

--- frontend/src/components/card/Card.tsx
+++ frontend/src/components/card/Card.tsx
@@ -1,30 +1,27 @@
 import React, { SyntheticEvent } from "react";
-import "./card.css";
 import { CompanySearch } from "../../api";
-
 import AddPortfolio from "../portifolio/addPortifolio/addPortifolio";
 
 interface Props {
   id: string;
   searchResult: CompanySearch;
-  // 1. Adicionou a função de criação nas Props do Card
   onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
 const Card: React.FC<Props> = ({ id, searchResult, onPortfolioCreate }: Props): React.JSX.Element => {
   return (
     <div
-      className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
+      className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row shadow-sm mb-3"
       key={id}
       id={id}
     >
-      <div className="details">
-        <h2 className="font-bold text-center text-veryDarkViolet md:text-left">
-          {searchResult.description} ({searchResult.symbol})
+      <div className="flex flex-col">
+        <h2 className="font-bold text-center text-veryDarkViolet md:text-left text-lg">
+          {searchResult.description || searchResult.description} ({searchResult.symbol})
         </h2>
-        
       </div>
-      {/* 2. Adicionou o componente AddPortfolio passando as props necessárias */}
+      
+      {/* O componente de botão fica do lado direito em telas maiores (md:flex-row) */}
       <AddPortfolio
         onPortfolioCreate={onPortfolioCreate}
         symbol={searchResult.symbol}
--- frontend/src/components/portifolio/addPortifolio/addPortifolio.tsx
+++ frontend/src/components/portifolio/addPortifolio/addPortifolio.tsx
@@ -5,17 +5,19 @@ interface Props {
   symbol: string;
 }
 
-const AddPortfolio = ({ onPortfolioCreate, symbol }: Props) => {
+const AddPortfolio: React.FC<Props> = ({ onPortfolioCreate, symbol }: Props) => {
   return (
-    <form onSubmit={onPortfolioCreate}>
-      <input readOnly={true} hidden={true} value={symbol} />
-      <button
-        type="submit"
-        className="p-2 px-8 text-white bg-darkBlue rounded-lg hover:opacity-70 focus:outline-none"
-      >
-        Add
-      </button>
-    </form>
+    <div className="flex flex-col items-center justify-end flex-1">
+      <form onSubmit={onPortfolioCreate}>
+        <input readOnly={true} hidden={true} value={symbol} />
+        <button
+          type="submit"
+          className="p-2 px-8 text-white bg-darkBlue rounded-lg hover:opacity-70 focus:outline-none font-semibold transition-all"
+        >
+          Add
+        </button>
+      </form>
+    </div>
   );
 };
 
